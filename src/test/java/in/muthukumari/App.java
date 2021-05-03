1541aae05297f400e19010bfc95aabbd9237f36b
MuthuKumari-P <82449715+MuthuKumari-P@users.noreply.github.com>
2021-05-03 17:22:53 +0530

    Update App.java

--- src/test/java/in/muthukumari/App.java
+++ src/test/java/in/muthukumari/App.java
@@ -1,5 +1,46 @@
-package in.muthukumari;
+package in.muthukumari.transfermoney;
+import static org.junit.Assert.assertEquals;
 
-public class App {
+import org.junit.Test;
+
+public class TesterForListOfBank {
+
+	/**
+	 * TestCase for no bank name
+	 */
+	@Test
+	public void testCase1() {
+		OperationsOnBankList.displayBankList();
+		int noOfBanks = OperationsOnBankList.getNoOfBanks();
+		System.out.println(noOfBanks);
+
+	}
+
+	@Test
+	public void testCase2() {
+		String bankName = " Indian bank";
+		boolean bankNameValidation = CustomerBankDetailValidation.nameValidation(bankName);
+		assertEquals(false,bankNameValidation);
+		//OperationsOnBankList.addBankList(bankName);
+		//OperationsOnBankList.displayBankList();
+		//int noOfBanks = OperationsOnBankList.getNoOfBanks();
+		//System.out.println(noOfBanks);
+
+	}
+
+	@Test
+	public void testCase3() {
+		String bankName = "Indian Overseas Bank";
+		boolean bankNameValidation = CustomerBankDetailValidation.nameValidation(bankName);
+		assertEquals(true,bankNameValidation);
+	}
+
+	@Test
+	public void testCase4() {
+		String bankName = " ";
+		boolean bankNameValidation = CustomerBankDetailValidation.nameValidation(bankName);
+		assertEquals(false,bankNameValidation);
+		}
+}
 
 }
